3f30b7bcf453e9c6b5899cd5e459595da342ff2c
Cory Hale <corylhale2013@gmail.com>
2020-03-21 12:34:38 -0600

    Finished hashtables

--- src/hashtable.py
+++ src/hashtable.py
@@ -54,8 +54,24 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)    
 
+        if self.storage[index]:
+            cur_node = self.storage[index]
+            found = False
+            while cur_node is not None and found is False:
+                if cur_node.key == key:
+                    cur_node.value = value
+                    found = True
+                else:
+                    cur_node = cur_node.next
+            if found == False:
+                cur_node = self.storage[index]
+                new_node = LinkedPair(key, value)
+                new_node.next = cur_node
+                self.storage[index] = new_node
+        else:
+            self.storage[index] = LinkedPair(key, value)
 
 
     def remove(self, key):
@@ -66,7 +82,40 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+
+        if self.storage[index]:
+            prev_node = None
+            cur_node = self.storage[index]
+            found = False
+            while cur_node is not None and found is False:
+                if prev_node is None and cur_node.next is None:
+                    if cur_node.key == key:
+                        self.storage[index] = None
+                        found = True
+                    else:
+                        prev_node = cur_node
+                        cur_node = cur_node.next
+                else:
+                    if cur_node.key == key:
+                        if prev_node is None:
+                            prev_node = cur_node
+                            cur_node = cur_node.next
+                            prev_node.next = None
+                            self.storage[index] = cur_node
+                        elif cur_node.next is None:
+                            prev_node.next = None
+                        else:
+                            prev_node.next = cur_node.next
+                            cur_node.next = None
+                        found = True
+                    else:
+                        prev_node = cur_node
+                        cur_node = cur_node.next
+            if found == False:
+                print("Warning: key does not exist.")
+        else:
+            print("Warning: key does not exist.")
 
 
     def retrieve(self, key):
@@ -77,7 +126,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+
+        if self.storage[index]:
+            cur_node = self.storage[index]
+            while cur_node is not None:
+                if cur_node.key == key:
+                    return cur_node.value
+                else:
+                    cur_node = cur_node.next
+
+        return None
 
 
     def resize(self):
@@ -87,7 +146,42 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        old_storage = self.storage
+        self.capacity = 2 * self.capacity
+        self.storage = [None] * self.capacity
+
+        current_pair = None
+
+        for bucket_item in old_storage:
+            current_pair = bucket_item
+            while current_pair is not None:
+                self.insert(current_pair.key, current_pair.value)
+                current_pair = current_pair.next
+
+
+        # storage_copy = self.storage
+        # current = None
+        # self.capacity = self.capacity * 2
+        # self.storage = [None] * self.capacity
+
+        # for i in range(0, len(storage_copy)):
+        #     current = storage_copy[i]
+        #     while current is not None:
+        #         self.insert(current.key, current.value)
+        #         current = current.next
+            
+
+        # new_arr = [None] * self.capacity
+        
+        # for i in range(0, len(self.storage)):
+        #     new_arr[i] = self.storage[i]
+        #     print(self.storage[i])
+
+        # self.capacity = self.capacity * 2
+
+        # self.storage = [None] * self.capacity
+        # for i in range(0, len(new_arr)):
+        #     self.storage[i] = new_arr[i]
 
 
 
